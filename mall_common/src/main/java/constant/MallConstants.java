0f7c0ce758f0fc2c8da2bccf64183be95a32ff69
D_h <54danghao@gmail.com>
2020-06-03 21:24:38 +0800

    公用包

---
+++ mall_common/src/main/java/constant/MallConstants.java
@@ -0,0 +1,15 @@
+package constant;
+
+public class MallConstants {
+	/** 启动日志 */
+	public static final String KAFKA_TOPIC_STARTUP="MALL-STARTUP";
+	/** 事件日志 */
+	public static final String KAFKA_TOPIC_EVENT="MALL-EVENT";
+	public static final String KAFKA_TOPIC_NEW_ORDER="MALL-NEW_ORDER";
+	public static final String KAFKA_TOPIC_ORDER_DETAIL="MALL-ORDER-DETAIL";
+
+	public static final String ES_INDEX_ALTER="mall_coupon_alter";
+	public static final String ES_INDEX_NEW_MID="mall_new_mid";
+	public static final String ES_INDEX_NEW_ORDER="mall_new_order";
+	public static final String ES_INDEX_SALE_DETAIL="mall_sale_detail";
+}
