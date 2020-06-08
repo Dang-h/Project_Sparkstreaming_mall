f16f45cc2f9b37de0265e74dfdf0cccfa76be46f
D_h <54danghao@gmail.com>
2020-06-08 16:33:26 +0800

    canal客户端

---
+++ mall_canal/src/main/java/canal/client/CanalClient.java
@@ -0,0 +1,72 @@
+package canal.client;
+
+import canal.handler.CanalHandler;
+import com.alibaba.otter.canal.client.CanalConnector;
+import com.alibaba.otter.canal.client.CanalConnectors;
+import com.alibaba.otter.canal.protocol.CanalEntry;
+import com.alibaba.otter.canal.protocol.Message;
+import com.google.protobuf.InvalidProtocolBufferException;
+
+import java.net.InetSocketAddress;
+import java.util.List;
+
+public class CanalClient {
+	public static void main(String[] args) {
+
+		//创建连接器,与canal的服务端取得连接
+		CanalConnector canalConnector = CanalConnectors.newSingleConnector(new InetSocketAddress("hadoop100", 11111), "example", "", "");
+
+		while (true) {
+			//建立连接
+			canalConnector.connect();
+
+			//订阅,监控sparkStreaming_mall库下所有表
+			canalConnector.subscribe("sparkStreaming_mall.*");
+
+			//抓取100个entry相当于100条sql，抓取100份数据
+			Message message = canalConnector.get(100);
+
+			//如果没有抓取到数据，冷静5秒
+			if (message.getEntries().size() == 0) {
+				System.out.println("我想静静");
+				try {
+					Thread.sleep(5000);
+				} catch (InterruptedException e) {
+					e.printStackTrace();
+				}
+			} else { //有数据，单独处理
+
+				//提取entry数据
+				for (CanalEntry.Entry entry : message.getEntries()) {
+
+					//只有行变化才处理
+					if (entry.getEntryType().equals(CanalEntry.EntryType.ROWDATA)) {
+						CanalEntry.RowChange rowChange = null;
+
+						try {
+							//将entry里的数据反序列化，
+							rowChange = CanalEntry.RowChange.parseFrom(entry.getStoreValue());
+						} catch (InvalidProtocolBufferException e) {
+							e.printStackTrace();
+						}
+
+						//得到rowDataList
+						List<CanalEntry.RowData> rowDatasList = rowChange.getRowDatasList();
+						//得到操作的表,不同的表可能需要发送到不同的topic中
+						String tableName = entry.getHeader().getTableName();
+						//得到操作的类型
+						CanalEntry.EventType eventType = rowChange.getEventType();
+
+						CanalHandler canalHandler = new CanalHandler(tableName, eventType, rowDatasList);
+
+						canalHandler.handle();
+					}
+
+				}
+
+			}
+
+
+		}
+	}
+}
---
+++ mall_canal/src/main/java/canal/handler/CanalHandler.java
@@ -0,0 +1,70 @@
+package canal.handler;
+
+import canal.utils.MyKafkaSender;
+import com.alibaba.fastjson.JSONObject;
+import com.alibaba.otter.canal.protocol.CanalEntry;
+import constant.MallConstants;
+
+import java.util.List;
+
+
+//业务的具体处理
+public class CanalHandler {
+
+	//定义具体参数
+	//表名
+	String tableName;
+	//操作类型
+	CanalEntry.EventType eventType;
+	//数据列表
+	List<CanalEntry.RowData> rowDataList;
+
+	/**
+	 *
+	 * @param  tableName   表名
+	 * @param  eventType   操作类型
+	 * @param  rowDataList 数据
+	 * @return
+	 */
+	public CanalHandler(String tableName, CanalEntry.EventType eventType, List<CanalEntry.RowData> rowDataList) {
+		this.tableName = tableName;
+		this.eventType = eventType;
+		this.rowDataList = rowDataList;
+	}
+
+
+	//根据业务类型类，发送到不同的kafka主题
+	public void handle() {
+
+		//将不同的表的变化切分成不同的主题保存到对应的topic里
+		if (tableName.equals("order_info") && eventType.equals(CanalEntry.EventType.INSERT)) {
+			//处理数据
+			for (CanalEntry.RowData rowData : rowDataList) {
+				//发送数据
+				sendKafka(rowData, MallConstants.KAFKA_TOPIC_NEW_ORDER);
+			}
+		}
+	}
+
+	/**
+	 * 发送数据到Kafka对应的Topic中
+	 * @param rowData 行数据
+	 * @param topic   发送到的kafka的主题
+	 */
+	private void sendKafka(CanalEntry.RowData rowData, String topic) {
+
+		List<CanalEntry.Column> afterColumnsList = rowData.getAfterColumnsList();
+
+		JSONObject jsonObject = new JSONObject();
+
+		for (CanalEntry.Column column : afterColumnsList) {
+			System.out.println(column.getName() + " ---> " + column.getValue());
+
+			//把每一个字段变成一个JSON字符串,并发送至Kafka对应的topic中
+			jsonObject.put(column.getName(), column.getValue());
+		}
+		String rowJSON = jsonObject.toJSONString();
+		MyKafkaSender.send(topic, rowJSON);
+	}
+}
+
